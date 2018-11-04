b278fd3f779cdc16a260233cc0225f7e0c4fbd14
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-04 20:47:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,13 @@
-if Time.now.hour < 12
-  resp.write("Morning")
-else
-  resp.write("Afternoon")
+
+
+class Appilication 
+  def call(env)
+    resp = Rack::Response.new
+    if Time.now.hour < 12
+      resp.write("Morning")
+    else
+      resp.write("Afternoon")
+    end
+    resp.finished
+  end
 end
